77c2b254c96b76820ec5cc2a6988ce78c17b447c
dolianich <69437697+dolianich@users.noreply.github.com>
2024-08-19 12:22:25 +0300

    Custom Error Handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -10,11 +11,16 @@ connectDB();
 
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 4000;
