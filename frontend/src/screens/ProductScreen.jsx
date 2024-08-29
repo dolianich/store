8a60b3e2773c3ac2e82e0a53328542a06ccbc78b
dolianich <69437697+dolianich@users.noreply.github.com>
2024-08-29 15:26:48 +0300

    Product Details Reducer

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
 
 const ProductScreen = () => {
 
